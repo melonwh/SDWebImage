660477cb8c7f2d424f111e4f24f25d972ecd348c
张静 <zhangjing@youhaodongxi.com>
2018-04-18 11:06:37 +0800

    Zjtest

--- SDWebImage/UIImageView+WebCache.m
+++ SDWebImage/UIImageView+WebCache.m
@@ -3,7 +3,7 @@
  * (c) Olivier Poitrey <rs@dailymotion.com>
  *
  * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * file that was distributed with this source code.zjtest
  */
 
 #import "UIImageView+WebCache.h"
